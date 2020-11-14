25aec07dd3fdc3bbbc34a8030f9245086442d8b1
Fernando Bernal <statefull@gmail.com>
2020-11-14 16:14:45 +0100

    new function summarion

--- packages/advanced-operations/advanced.js
+++ packages/advanced-operations/advanced.js
@@ -1,8 +1,19 @@
-const multiply = require('@testing-lerna/basics-operations').multiply;
+const basicOperations = require('@testing-lerna/basics-operations');
 
 function factorial(a) {  
   if (a === 1) return 1;
-  return multiply (a, factorial(a -1));
+  return basicOperations.multiply (a, factorial(a -1));
+}
+
+function summation(value, times) {
+  let result = value;
+
+  for (let i = 0; i < times; ++i) {
+    result = basicOperations.add(result, value);
+  }
+  
+  return result;
 }
 
 exports.factorial = factorial;
+exports.summation = summation;
